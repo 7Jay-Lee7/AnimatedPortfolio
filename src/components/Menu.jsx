13cd01500d59a91f7b4dd50da8def8319e72e11c
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-07-04 19:09:52 +0100

    Add files via upload

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,8 @@
+import About from "../assets/About.png";
+import Contact from "../assets/Contact.png";
+import Projects from "../assets/Projects.png";
+import Skills from "../assets/Skills.png";
+
 export const Menu = (props) => {
   const { onSectionChange, menuOpened, setMenuOpened } = props;
 
@@ -28,10 +33,10 @@ export const Menu = (props) => {
       ${menuOpened ? "w-full md:w-80" : "w-0"}`}
       >
         <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-          <MenuButton label="About" onClick={() => onSectionChange(0)} />
-          <MenuButton label="Skills" onClick={() => onSectionChange(1)} />
-          <MenuButton label="Projects" onClick={() => onSectionChange(2)} />
-          <MenuButton label="Contact" onClick={() => onSectionChange(3)} />
+          <MenuButton label="About" imageSrc={About} onClick={() => onSectionChange(0)} />
+          <MenuButton label="Skills" imageSrc={Skills} onClick={() => onSectionChange(1)} />
+          <MenuButton label="Projects" imageSrc={Projects} onClick={() => onSectionChange(2)} />
+          <MenuButton label="Contact" imageSrc={Contact} onClick={() => onSectionChange(3)} />
         </div>
       </div>
     </>
@@ -39,13 +44,14 @@ export const Menu = (props) => {
 };
 
 const MenuButton = (props) => {
-  const { label, onClick } = props;
+  const { label, imageSrc, onClick } = props;
   return (
     <button
       onClick={onClick}
-      className="text-2xl font-bold cursor-pointer hover:text-indigo-950 transition-colors"
+      className="text-2xl font-bold cursor-pointer hover:text-indigo-950 transition-colors flex items-center"
     >
-      {label}
+      <img src={imageSrc} alt={label} className="mr-2 h-8 w-8" />
+      <span>{label}</span>
     </button>
   );
 };
