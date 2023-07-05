35245d8c14bb8559f55624aa9c2f96f80c306c53
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-07-05 18:43:47 +0100

    Add files via upload

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,77 +1,75 @@
 import { useEffect, useRef, useState } from "react";
-
-const CURSOR_SPEED = 0.08;
-
-let mouseX = -10;
-let mouseY = -10;
-let outlineX = 0;
-let outlineY = 0;
+import Shuriken from "../assets/Shuriken.png";
 
 export const Cursor = () => {
   const cursorOutline = useRef();
   const [hoverButton, setHoverButton] = useState(false);
+  let cursorX = -10;
+  let cursorY = -10;
 
   const animate = () => {
-    let distX = mouseX - outlineX;
-    let distY = mouseY - outlineY;
-
-    outlineX = outlineX + distX * CURSOR_SPEED;
-    outlineY = outlineY + distY * CURSOR_SPEED;
-
-    cursorOutline.current.style.left = `${outlineX}px`;
-    cursorOutline.current.style.top = `${outlineY}px`;
+    cursorOutline.current.style.left = `${cursorX}px`;
+    cursorOutline.current.style.top = `${cursorY}px`;
     requestAnimationFrame(animate);
   };
 
   useEffect(() => {
-    const mouseEventsListener = document.addEventListener(
-      "mousemove",
-      function (event) {
-        mouseX = event.pageX;
-        mouseY = event.pageY;
-      }
-    );
+    const mouseMoveListener = (event) => {
+      cursorX = event.pageX;
+      cursorY = event.pageY;
+    };
+
+    document.addEventListener("mousemove", mouseMoveListener);
+
     const animateEvent = requestAnimationFrame(animate);
+
     return () => {
-      document.removeEventListener("mousemove", mouseEventsListener);
+      document.removeEventListener("mousemove", mouseMoveListener);
       cancelAnimationFrame(animateEvent);
     };
   }, []);
 
   useEffect(() => {
-    const mouseEventListener = document.addEventListener(
-      "mouseover",
-      function (e) {
-        if (
-          e.target.tagName.toLowerCase() === "button" ||
-          // check parent is button
-          e.target.parentElement.tagName.toLowerCase() === "button" ||
-          // check is input or textarea
-          e.target.tagName.toLowerCase() === "input" ||
-          e.target.tagName.toLowerCase() === "textarea"
-        ) {
-          setHoverButton(true);
-        } else {
-          setHoverButton(false);
-        }
+    const mouseOverListener = (e) => {
+      const targetTagName = e.target.tagName.toLowerCase();
+      const parentTagName =
+        e.target.parentElement && e.target.parentElement.tagName.toLowerCase();
+
+      if (
+        targetTagName === "button" ||
+        parentTagName === "button" ||
+        targetTagName === "input" ||
+        targetTagName === "textarea"
+      ) {
+        setHoverButton(true);
+      } else {
+        setHoverButton(false);
       }
-    );
+    };
+
+    document.addEventListener("mouseover", mouseOverListener);
+
     return () => {
-      document.removeEventListener("mouseover", mouseEventListener);
+      document.removeEventListener("mouseover", mouseOverListener);
     };
   }, []);
 
   return (
     <>
       <div
-        className={`invisible md:visible  z-50 fixed -translate-x-1/2 -translate-y-1/2 rounded-full pointer-events-none transition-transform
-        ${
-          hoverButton
-            ? "bg-transparent border-2 border-indigo-900 w-5 h-5"
-            : "bg-red-950 w-3 h-3"
+        className={`invisible md:visible z-50 fixed -translate-x-1/2 -translate-y-1/2 rounded-full pointer-events-none transition-transform ${
+          hoverButton ? "bg-transparent border-2 border-indigo-900 w-5 h-5" : ""
         }`}
         ref={cursorOutline}
-      ></div>
+      >
+        {hoverButton ? null : (
+          <img
+            src={Shuriken}
+            alt="Shuriken"
+            className="shuriken-spin" // Apply a CSS class for the spinning animation
+          />
+        )}
+      </div>
     </>
   );
 };
